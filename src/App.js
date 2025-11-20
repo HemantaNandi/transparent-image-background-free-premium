840d16da6ef874f6e373dabd684e2f7b65e1ce7a
Hemanta Nandi <hemantanandi2@gmail.com>
2025-11-21 00:09:57 +0530

    chore: update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import BackgroundRemover from './components/BackgroundRemover';
 
 function App() {
   useEffect(() => {
-    document.title = 'Free Image Background Remover | transparent.pics';
+    document.title = 'Free AI Image Background Remover | transparent.pics';
   }, []);
   return (
     <div className="App">
       <header className="App-header">
         <div className="header-content">
           <h1 className="App-title-text">transparent.pics</h1>
-          <h2>Image Background Remover</h2>
+          <h2>Free AI Image Background Remover</h2>
         </div>
       </header>
       <main>
